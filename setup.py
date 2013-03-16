8247b8c760e7afb305f8220e561d552ee8926daa
Kristofer M White <me@kmwhite.net>
2013-03-16 03:33:10 -0500

    Adding python package config

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+setup(
+    name = "artisan",
+    version = "0.0.0",
+    packages = find_packages(),
+    scripts = [],
+
+    install_requires = [
+        "python-faker >= 0.2.4",
+    ],
+
+    package_data = {
+        'src/artisan': ['*.py'],
+    },
+
+    # metadata for upload to PyPI
+    author = "Kristofer M White",
+    author_email = "me@kmwhite.net",
+    description = "A python testing library to help craft better tests",
+    license = "BSD",
+    keywords = "tdd testing unit unittest",
+    url = "http://kmwhite.github.com/artisan/",
+)
