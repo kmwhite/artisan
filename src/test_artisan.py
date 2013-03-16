1db8a158bd2bfb876ec3a9ce28e35da89b1a9c7a
Kristofer M White <me@kmwhite.net>
2013-03-16 14:48:23 -0500

    Updating tests to cover improvements to Artisan

--- src/test_artisan.py
+++ src/test_artisan.py
@@ -1,22 +1,40 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
+
 import artisan
 import blueprints
 
+# Testing simple class creation
 class Foo(object):
     ''' Some Class '''
-    name = None
+
+    def __init__(self, *args):
+        name = None
 
 artisan.prepare(blueprints)
 
 print('Testing instance creation')
 for num in range(10):
     new_instance = artisan.craft(Foo)
-    print(new_instance.name)
+    assert new_instance.name is not None
 
 print('Testing Keyword Overrides')
 for num in range(10):
     new_instance = artisan.craft(Foo, name='FooBarBaz')
-    print(new_instance.name)
+    assert new_instance.name == 'FooBarBaz'
 
+# Testing more complex class creation
+class Bar(object):
+    ''' Some Other Class '''
 
+    def __init__(self, an_argument):
+        self.random_attrib = an_argument
+        self.name = None
+
+print('Testing class creation')
+for num in range(10):
+    name = "FooBarBaz_{}".format(num)
+    new_instance = artisan.craft(Bar, num, name=name)
+    assert new_instance.random_attrib == num
+    assert new_instance.name == name
