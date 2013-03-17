99effc4fcffe10abc330ca1de9145bcb9173d7c4
Kristofer M White <me@kmwhite.net>
2013-03-17 14:06:15 -0500

    Cleaning up tests to make them be proper unittests

--- src/test_artisan.py
+++ src/test_artisan.py
@@ -2,29 +2,17 @@
 
 from __future__ import absolute_import, print_function
 
+import unittest
+
 import artisan
 import tests.blueprints
 
-# Testing simple class creation
 class Foo(object):
     ''' Some Class '''
 
     def __init__(self, *args):
         name = None
 
-artisan.prepare(tests.blueprints)
-
-print('Testing instance creation')
-for num in range(10):
-    new_instance = artisan.craft(Foo)
-    assert new_instance.name is not None
-
-print('Testing Keyword Overrides')
-for num in range(10):
-    new_instance = artisan.craft(Foo, name='FooBarBaz')
-    assert new_instance.name == 'FooBarBaz'
-
-# Testing more complex class creation
 class Bar(object):
     ''' Some Other Class '''
 
@@ -32,9 +20,30 @@ class Bar(object):
         self.random_attrib = an_argument
         self.name = None
 
-print('Testing class creation')
-for num in range(10):
-    name = "FooBarBaz_{}".format(num)
-    new_instance = artisan.craft(Bar, num, name=name)
-    assert new_instance.random_attrib == num
-    assert new_instance.name == name
+artisan.prepare(tests.blueprints)
+
+class TestArtisan(unittest.TestCase):
+
+    def setUp(self):
+        ''' Set up the tests '''
+        pass
+
+    def tearDown(self):
+        ''' Tear Down the tests '''
+        pass
+
+    def test_instance_creation(self):
+        for num in range(10):
+            new_instance = artisan.craft(Foo)
+            assert new_instance.name is not None
+
+    def test_instance_creation_with_arguments(self):
+        for num in range(10):
+            name = "FooBarBaz_{}".format(num)
+            new_instance = artisan.craft(Bar, num, name=name)
+            assert new_instance.random_attrib == num
+            assert new_instance.name == name
+
+    def test_keyword_overrides(self):
+        new_instance = artisan.craft(Foo, name='FooBarBaz')
+        assert new_instance.name == 'FooBarBaz'
